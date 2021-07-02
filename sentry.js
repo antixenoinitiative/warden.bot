521234b45f9b6fe51058612ad9041fea54522b8c
CMDRMgram <marcus@ingrams.cloud>
2021-07-02 22:10:33 +1000

    Test JSON Parse

--- sentry.js
+++ sentry.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-request('http//exmaple.com/sample_data.json', function (error, response, body) {
+request('https://cms.zaonce.net/en-GB/jsonapi/node/galnet_article', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var importedJSON = JSON.parse(body);
      console.log(importedJSON);
