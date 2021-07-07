6f76df1b2db22c2bf82536b98feb9a6f5bc736b4
Marcus Ingram <marcus@ingrams.cloud>
2021-07-08 01:25:57 +1000

    Basic Listener
    Developing Basic EDDN Listener

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import zlib from 'zlib';
-import zmq from 'zeromq';
+import zlib from 'zlib';                                                          //EDDN Listener Dependency
+import zmq from 'zeromq';                                                         //EDDN Listener Dependency
 
-const SOURCE_URL = 'tcp://eddn.edcd.io:9500';
+const SOURCE_URL = 'tcp://eddn.edcd.io:9500';                                     //EDDN Data Stream URL
+const targetstate = "Incursion";                                                  //The current system state to check for (Incursion)
 
-async function run() {
+async function run() { 
   const sock = new zmq.Subscriber;
 
   sock.connect(SOURCE_URL);
   sock.subscribe('');
   console.log('EDDN listener connected to:', SOURCE_URL);
 
-  for await (const [src] of sock) {
-    const msg = JSON.parse(zlib.inflateSync(src));
-    console.log(msg);
+  for await (const [src] of sock) {                                               //for Each Listener Message
+    const msg = JSON.parse(zlib.inflateSync(src));                                //convert the message to JSON
+    const sysstate = msg.message.StationFaction?.FactionState;                    //grab the Faction State and store it in a sysstate
+    if (sysstate == targetstate) {                                                //if the System state is equal to the Target State
+      console.log(`${targetstate} in ${msg.message.StarSystem}`);                 //send "<target state> is in <system name>" to console
+    }
   }
 }
 
