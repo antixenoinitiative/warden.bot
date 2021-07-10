fdce7233b23eeff0ff2e140dda59ab9edfc44d56
FeroxA <66964414+FeroxA@users.noreply.github.com>
2021-07-10 18:10:44 +0100

    added bearbones API code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const zlib = require("zlib");
 const zmq = require("zeromq");
-const { Pool } = require('pg')
+const { Pool } = require('pg');
+const api = require('express')();
 
 require("dotenv").config();
 
+
 const SOURCE_URL = 'tcp://eddn.edcd.io:9500'; //EDDN Data Stream URL
 const targetstate = "Boom"; //The current system state to check for (Incursion)
+let msg;
+
+console.log(mesAGE.header.timestamp);
 
 const pool = new Pool({ //credentials stored in .env file
   user: process.env.DBUSER,
@@ -22,21 +27,44 @@ async function run() {
   console.log('EDDN listener connected to:', SOURCE_URL);
 
   for await (const [src] of sock) {
-    const msg = JSON.parse(zlib.inflateSync(src));
+    msg = JSON.parse(zlib.inflateSync(src));
+
+    // console.log(msg);
     if (msg.$schemaRef == "https://eddn.edcd.io/schemas/journal/1") { //only process correct schema
       const sysstate = msg.message.StationFaction?.FactionState;
       if (sysstate == targetstate) {
         console.log(`${msg.message.timestamp}: ${targetstate} detected in system: ${msg.message.StarSystem}`);
-        pool.query("SELECT * FROM systems where name = 'msg.message.StarSystem'", (err, res) => {
-          if (res.rowCount == 1) {
-            console.log(`${msg.message.StarSystem} exists in the Database`);
-          } else {
-            console.log(`${msg.message.StarSystem} does not exist in the Database`);
-          }
-        })
+
+        // pool.query("SELECT * FROM systems where name = 'msg.message.StarSystem'", (err, res) => {
+        //   if (res.rowCount == 1) {
+        //     console.log(`${msg.message.StarSystem} exists in the Database`);
+        //   } else {
+        //     console.log(`${msg.message.StarSystem} does not exist in the Database`);
+        //   }
+        // });
       }
     }
   }
 }
 
+// TEST API CODE
+api.listen(3000, () => { 
+  console.log('AXI Sentry is operational');  // Upon a successful connection will log to console
+});
+
+api.get('/', (req, res) => res.json(  // When a request is made to the base dir, call the callback function json()
+  {
+    header: { // Contains data about the message
+      timestamp: `${new Date().toISOString()}`, // Sets timestamp to the current time in ISO8601 format.
+      softwareName: 'AXI Sentry', // Name of API
+      softwareVersion: '0.1',  // Arbituary number currently
+    },
+    message: { // The actual content of the message 
+      system: `${msg.message.timestamp}: ${targetstate} detected in system: ${msg.message.StarSystem}`   
+    }
+  }
+  ),
+);
+
 run();
+
