9d564161b4458c046d6cdeac3faa8584d4b64089
CMDRMgram <marcus@ingrams.cloud>
2021-07-14 00:45:32 +1000

    Added Incursion logging and some API stuff

--- index.js
+++ index.js
@@ -37,9 +37,35 @@ async function QuerySelect (column1, table, column2, value) {
   return res;
 }
 
+async function GetIncursions () {
+  const client = await pool.connect();
+  let res;
+  try {
+    await client.query('BEGIN');
+    try {
+      res = await client.query(`SELECT systems.system_id, systems.name,incursions.inc_id, incursions.date FROM incursions INNER JOIN systems ON incursions.system_id=systems.system_id;`);
+      await client.query('COMMIT');
+    } catch (err) {
+      console.log(err);
+      await client.query('ROLLBACK');
+      throw err;
+    }
+  } finally {
+    client.release();
+  }
+  return res;
+}
+
 // Add a system to DB
-function AddSystem (name) {
+async function AddSystem (name) {
   pool.query(`INSERT INTO systems(name)VALUES($1)`,[name],(err, res) => { //$1 is untrusted and sanitized
+    //console.log(err);
+  });
+}
+
+// Add a system to DB
+function AddIncursion (id) {
+  pool.query(`INSERT INTO incursions(system_id)VALUES($1)`,[id],(err, res) => { //$1 is untrusted and sanitized
     //console.log(err);
   });
 }
@@ -54,6 +80,15 @@ async function GetSysID (name) {
   }
 }
 
+async function GetIncID (id) { 
+  try {
+    const { rows } = await QuerySelect("inc_id", "incursions", "system_id", id);
+    return rows[0].inc_id; // Return System_id
+  } catch (err) {
+    return 0; // Return 0 if system is not in the DB
+  }
+}
+
 // Primary Function
 async function run() {
 
@@ -62,7 +97,6 @@ async function run() {
   sock.connect(SOURCE_URL);
   sock.subscribe('');
   console.log("[✔] EDDN Listener Connected: ", SOURCE_URL);
-
   for await (const [src] of sock) {
 
     msg = JSON.parse(zlib.inflateSync(src));
@@ -71,16 +105,19 @@ async function run() {
     if (msg.$schemaRef == "https://eddn.edcd.io/schemas/journal/1") { //only process correct schema
       const systemState = StationFaction?.FactionState;
       if (systemState == targetState) {
-
+        
         if (await GetSysID(StarSystem) == 0) { // Check if the system is in the DB
 
           await AddSystem(StarSystem); // Add the System to DB
-          console.log(`[${timestamp}]: ADDED [${StarSystem}] ID: ` + await GetSysID(StarSystem)); // Log the ID of the system added to DB
-
+          console.log(`[${timestamp}]: ADDED [${StarSystem}]`); // Log the ID of the system added to DB
         } else {
-
-          console.log(`[${timestamp}]: EXISTS [${StarSystem}] ID: ` + await GetSysID(StarSystem));
+          console.log(`[${timestamp}]: EXISTS [${StarSystem}]`);
         }
+
+        let SysID = await GetSysID(StarSystem);
+        await AddIncursion(await GetSysID(StarSystem)); // Log the Incursion to DB
+        console.log(`[${timestamp}]: LOGGED INCURSION ID: ` + await GetIncID(SysID));
+        
       } else {
         //console.log(`[${timestamp}]: SKIPPED [${StarSystem}]`);
       }
@@ -104,6 +141,22 @@ api.get('/', (req, res) => res.json(  // When a request is made to the base dir,
       }
     }
   ),
+);
+
+api.get('/all_incursions', async function(req, res) {
+  const { rows } = await GetIncursions()
+  res.json(
+    {
+      header: {
+        timestamp: `${new Date().toISOString()}`,
+        softwareName: 'AXI Sentry',
+        softwareVersion: '0.1',
+      },
+      message: {
+        incursions: rows, // The actual content of the message
+      }
+    })
+  },
 );
 
 run();
