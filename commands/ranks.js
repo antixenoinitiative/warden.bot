481d0dceb9bf80b6bef45c81644e822a1371bb4c
CMDRMgram <marcus@ingrams.cloud>
2021-08-03 18:29:40 +1000

    Update ranks.js

--- commands/ranks.js
+++ commands/ranks.js
@@ -7,23 +7,21 @@ module.exports = {
 	execute(message, args) {
         if (args == "challenge") {
             try {
-                wiki.search(args).then((res) => {
-                    const returnEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF7100')
-                    .setAuthor('The Anti-Xeno Initiative', "https://cdn.discordapp.com/attachments/860453324959645726/865330887213842482/AXI_Insignia_Hypen_512.png")
-                    .setTitle("**Challenge Ranks**")
-                    .setDescription(`Challenge Rank Statistics`)
-                    returnEmbed.addField("Caduceus' Glint", guild.roles.get("810410422871785472").members.size)
-                    returnEmbed.addField("Ace", guild.roles.get("650449319262158868").members.size)
-                    returnEmbed.addField("Astraea's Clarity", guild.roles.get("868809340788834324").members.size)
-                    returnEmbed.addField("Snake Eater", guild.roles.get("508638571565940736").members.size)
-                    returnEmbed.addField("Soaring Sleipnir", guild.roles.get("603345251192537098").members.size)
-                    returnEmbed.addField("Annihilator", guild.roles.get("528577192746287104").members.size)
-                    returnEmbed.addField("100% Club", guild.roles.get("477645690630307841").members.size)
-                    returnEmbed.addField("Myrmidon", guild.roles.get("810410728023916554").members.size)
-                    returnEmbed.addField("Vanguard", guild.roles.get("642840616694317104").members.size)
-                    message.channel.send(returnEmbed.setTimestamp());
-                })
+                const returnEmbed = new Discord.MessageEmbed()
+                .setColor('#FF7100')
+                .setAuthor('The Anti-Xeno Initiative', "https://cdn.discordapp.com/attachments/860453324959645726/865330887213842482/AXI_Insignia_Hypen_512.png")
+                .setTitle("**Challenge Ranks**")
+                .setDescription(`Challenge Rank Statistics`)
+                returnEmbed.addField("Caduceus' Glint", guild.roles.get("810410422871785472").members.size)
+                returnEmbed.addField("Ace", guild.roles.get("650449319262158868").members.size)
+                returnEmbed.addField("Astraea's Clarity", guild.roles.get("868809340788834324").members.size)
+                returnEmbed.addField("Snake Eater", guild.roles.get("508638571565940736").members.size)
+                returnEmbed.addField("Soaring Sleipnir", guild.roles.get("603345251192537098").members.size)
+                returnEmbed.addField("Annihilator", guild.roles.get("528577192746287104").members.size)
+                returnEmbed.addField("100% Club", guild.roles.get("477645690630307841").members.size)
+                returnEmbed.addField("Myrmidon", guild.roles.get("810410728023916554").members.size)
+                returnEmbed.addField("Vanguard", guild.roles.get("642840616694317104").members.size)
+                message.channel.send(returnEmbed.setTimestamp());
             } catch (err) {
                 message.channel.send(`Something went wrong. Error: ${err}`);
             }
