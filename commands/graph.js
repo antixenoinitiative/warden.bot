cf10f823fe0a2cc3838c4809d3ee442a60e14cc8
CMDRMgram <marcus@ingrams.cloud>
2021-08-06 10:18:18 +1000

    -updated graph

--- commands/graph.js
+++ commands/graph.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const { response } = require("express");
 
 const data = [
     {
@@ -32,22 +31,43 @@ const data = [
     },
 ]
 
+function isValid(args) {
+    for (i=0;i < data.length; i++) {
+        if (args == data[i].argument) {
+            return true;
+        }
+    }
+    return false;
+}
+
 module.exports = {
-    name: 'graphic',
+    name: 'graph',
     description: 'Request a graphic, diagram or resource from a repository, use "-graphic" to get a list.',
     permlvl: 0,
-    format: '"graphic name"',
+    format: '"graphname"',
     restricted: false,
     execute(message, args) {
         let response;
+        if (!isValid(args)) {
+            const returnEmbed = new Discord.MessageEmbed()
+            .setColor('#FF7100')
+            .setAuthor('The Anti-Xeno Initiative', "https://cdn.discordapp.com/attachments/860453324959645726/865330887213842482/AXI_Insignia_Hypen_512.png")
+            .setTitle("Graphics")
+            .setDescription("List of valid graphic commands")
+            for (i=0;i < data.length; i++) {
+                returnEmbed.addField(`-graph ${data[i].argument}`, data[i].title);
+            }
+            return message.channel.send(returnEmbed.setTimestamp());
+        }
+
         for (i=0;i < data.length; i++) {
-            if (args == data[i].title) {
+            if (args == data[i].argument) {
                 response = data[i];
             }
         }
 
         if (response.type == "text") {
-            message.channel.send(data.link);
+            message.channel.send(response.link);
         } else if (response.type == "embed") {
             const returnEmbed = new Discord.MessageEmbed()
             .setColor('#FF7100')
@@ -57,7 +77,7 @@ module.exports = {
             .setImage(response.link)
             message.channel.send(returnEmbed.setTimestamp());
         } else {
-            message.channel.send("Please use a valid argument");
+            
         }
     }
 };
