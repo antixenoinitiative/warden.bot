a593b5d73a29cae47e4f6cc6ad0229606647c735
CMDRMgram <marcus@ingrams.cloud>
2021-08-07 20:06:48 +1000

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -15,15 +15,17 @@ module.exports = {
             {
                 throw("Role name too short. Add more letters to role names for best results.")
             }
-            roleID = getRoleID(message,role)
-            actualrole = cleanString(message.guild.roles.cache.find(role => role.id == roleID).name)
-            
+            let roleID = getRoleID(message,role)
+            let actualrole = cleanString(message.guild.roles.cache.find(role => role.id == roleID).name)
+            let membercount = message.guild.roles.cache.get(roleID).members.size
+
             const returnEmbed = new Discord.MessageEmbed()
             .setColor('#FF7100')
             .setAuthor('The Anti-Xeno Initiative', "https://cdn.discordapp.com/attachments/860453324959645726/865330887213842482/AXI_Insignia_Hypen_512.png")
             .setTitle(`**Role Info - ${actualrole}**`)
             returnEmbed.addField("Name", "```" + actualrole + "```", true)
             returnEmbed.addField("ID", "```" + roleID + "```", true)
+            returnEmbed.addField("Total Members", "```" + membercount + "```", true)
             message.channel.send(returnEmbed.setTimestamp());
 
         }
