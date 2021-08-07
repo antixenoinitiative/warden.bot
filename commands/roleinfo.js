aedb335be0adc2348261dcc486fead07bed76b4b
CMDRMgram <marcus@ingrams.cloud>
2021-08-07 18:48:09 +1000

    Create roleinfo.js

---
+++ commands/roleinfo.js
@@ -0,0 +1,18 @@
+const { getRoleID } = require("../discord/roles");
+
+module.exports = {
+	name: 'roleinfo',
+	description: 'Get information about a role',
+    format: '"role name"',
+	permlvl: 0,
+	restricted: false,
+	execute(message, args) {
+		try {
+            let roleinput = args.join(" ");
+            let role = getRoleID(message, roleinput);
+            message.channel.send(`${role}`)
+		} catch {
+			message.channel.send("Something went wrong, please you entered a correct term")
+		}
+	},
+};
