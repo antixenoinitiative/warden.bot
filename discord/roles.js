6812007644faf76d91868def82d2533a9df6d63c
CMDRMgram <marcus@ingrams.cloud>
2021-08-07 18:37:35 +1000

    Adding GetRoleID function

---
+++ discord/roles.js
@@ -0,0 +1,20 @@
+const compare = require("string-similarity");
+
+module.exports = {
+    /**
+     * Function takes a input string and returns the closest matching Server Role ID
+     * @param   {object} message    Pass through the message object
+     * @param   {string} name       The Role name you need to check
+     * @returns {string}            Returns the Role ID
+     */
+    getRoleID: (message, name) => {
+        let roleList = []
+        message.guild.roles.cache.forEach(role => {
+            if (role.name !='@everyone' && role.name != '@here') {
+                roleList.push(cleanString(role.name));
+            }
+        });
+        let best = compare.findBestMatch(name, roleList);
+        return message.guild.roles.cache.find(role => role.name == roleList[best["bestMatchIndex"]]).id.toString()
+    }
+}
