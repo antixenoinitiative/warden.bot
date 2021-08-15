e46aa7b05b138c81274060ecb98db2603a12ad01
FeroxA <66964414+FeroxA@users.noreply.github.com>
2021-08-15 16:36:39 +0100

    fixed issue #11

--- commands/admin/kill.js
+++ commands/admin/kill.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: 'kill',
 	description: 'Kills the AXI bot until it is rebooted (Auto-Reboot every 24 Hours)',
-  usage: '',
+  	usage: '',
 	permlvl: 2, // 0 = Everyone, 1 = Mentor, 2 = Staff
-	execute(message) {
-    message.channel.send({ content: "Killing Warden Bot! 💀" })
+	async execute(message) {
+    	await message.channel.send("Killing Warden Bot! 💀")
 		message.client.destroy();
-	},
+	}
 };
