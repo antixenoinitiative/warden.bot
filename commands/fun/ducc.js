b6fbbafe41e386d9ebc3f489237e59982794882c
MalzaCAr <88378003+MalzaCAr@users.noreply.github.com>
2021-08-26 17:40:59 +0200

    Update ducc.js

--- commands/fun/ducc.js
+++ commands/fun/ducc.js
@@ -6,7 +6,15 @@ module.exports = {
     permlvl: 0, // 0 = Everyone, 1 = Mentor, 2 = Staff
     hidden: true,
     execute (message) {
-        message.channel.send({ content: `You summoneth the ducc! <@211624816619290624>` });
-        message.channel.send({ content: "https://c.tenor.com/N3GVRxPTh-4AAAAM/duck-lmao.gif" });
+        var c = Math.random() * 100;
+        if (c < 50) {
+            message.channel.send({ content: `You summoneth the ducc! <@211624816619290624>` });
+            message.channel.send({ content: "https://c.tenor.com/N3GVRxPTh-4AAAAM/duck-lmao.gif" });
+        }
+        else {
+            message.channel.send({ content: `Who summons the duck-man?! <@211624816619290624>` });
+            message.channel.send({ content: "https://cdn.discordapp.com/attachments/625989888432537611/668012845925138442/duckswag.gif" });
+        }
+        
     }
 }
