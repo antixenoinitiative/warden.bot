78dd241b92b1f65b24fb6f08411f15a5af32bd16
Marcus Ingram <85346345+CMDRMgram@users.noreply.github.com>
2021-08-29 23:22:24 +1000

    Update ducc.js

--- commands/fun/ducc.js
+++ commands/fun/ducc.js
@@ -6,6 +6,15 @@ module.exports = {
     permlvl: 0, // 0 = Everyone, 1 = Mentor, 2 = Staff
     hidden: true,
     execute (message) {
+        if (message.author.id == "274853598280810496") {
+            var c = Math.random() * 100;
+            if (c < 50) {
+                message.reply({ content: `Oh no, not you again` });
+            }
+            else {
+                message.reply({ content: `Shoo... go away...` });
+            }
+        }
         var c = Math.random() * 100;
         if (c < 50) {
             message.channel.send({ content: `You summoneth the ducc! <@211624816619290624>` });
