5769741ff40d0210e8c6d77ddd691b258c49c752
henrygrv <66964414+FeroxA@users.noreply.github.com>
2021-08-31 11:41:42 +0100

    rule 3 command push test

--- commands/admin/rule3.js
+++ commands/admin/rule3.js
@@ -3,5 +3,17 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName(`nickname`)
-    .setDescription(`Changes your nickname to comply with server rules`)
+    .setDescription(`Changes your nickname to comply with server rules.`)
+
+    .addStringOption(option => option.setName(`in-game name`)
+        .setDescription(`Your username that appears in game.`)
+        .setRequired(true))
+
+    .addStringOption(option => option.setName(`squadron code`)
+        .setDescription(`The 4 character code of your squadron.`)
+        .setRequired(false)),
+
+    permissions: 0,
+
+    async execute(interaction)
 }
