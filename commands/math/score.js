0d12d1b3c6270a2462f31471e4a3b7bc759b0bf1
Astyrrean <FScognamiglio10@gsb.columbia.edu>
2021-09-01 10:46:13 -0400

    Update score.js

--- commands/math/score.js
+++ commands/math/score.js
@@ -213,6 +213,44 @@ module.exports = {
 
         let finalScore = targetRun - penaltyTotal
         
+        // Chart creation
+
+        <canvas id="myChart" width="400" height="400"></canvas>
+
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ['Vanguard', 'AmmoType', 'AmmoUsage', 'TimeTaken', 'DamageTaken'],
+            datasets: [{
+                label: 'Score',
+                data: [100 + vangPenaltyTotal, 100 + ammoPenalty , 100 + ammoPenalty, 100 + roundPenaltyTotal, 100 + hullPenaltyTotal],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 3
+            }]
+            },
+            options: {
+                scale: {
+                        // Hides the scale
+                        display: false
+                    }]
+                }
+            }
+        });
+
         // Print reply
         interaction.reply(`Thank you for submitting a New Ace score request!`)
         interaction.reply(`This score calculator is currently in Alpha and may change without notice`)
