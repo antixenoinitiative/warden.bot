a42bb8646c33d03e1e087a24243e59611a21bc1d
henrygrv <66964414+FeroxA@users.noreply.github.com>
2021-09-03 17:10:19 +0100

    minor rule 3 change

--- commands/admin/rule3.js
+++ commands/admin/rule3.js
@@ -21,7 +21,7 @@ module.exports = {
         .addChoice("No", "no"))
     .addUserOption(option => option.setName(`user`)
         .setDescription(`Which user's nickname would you like to change? (Admin-Only)`)
-        .setRequired(false),
+        .setRequired(false)
     .setDefaultPermission(false)),
     permissions: 2,
 
