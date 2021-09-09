43218d42eda827ee4d1058526ee823e9edd0ac1a
henrygrv <66964414+FeroxA@users.noreply.github.com>
2021-09-09 18:13:29 +0100

    rule3

--- commands/admin/rule3.js
+++ commands/admin/rule3.js
@@ -3,31 +3,33 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName(`nickname`)
-    .setDescription(`Changes your nickname to comply with server rules.`)
+        .setName(`nickname`)
+        .setDescription(`Changes your nickname to comply with server rules.`)
 
-    .addStringOption(option => option.setName(`ign`)
-        .setDescription(`Your username that appears in game.`)
-        .setRequired(true))
+        .addStringOption(option => option.setName(`ign`)
+            .setDescription(`Your username that appears in game.`)
+            .setRequired(true))
 
-    .addStringOption(option => option.setName(`squadroncode`)
-        .setDescription(`The 4 character code of your squadron.`)
-        .setRequired(false))
+        .addStringOption(option => option.setName(`squadroncode`)
+            .setDescription(`The 4 character code of your squadron.`)
+            .setRequired(false))
 
-    .addStringOption(option => option.setName(`vr`)
-        .setDescription(`Do you play in vr?`)
-        .setRequired(false)
-        .addChoice("Yes", "yes")
-        .addChoice("No", "no"))
-    .addUserOption(option => option.setName(`user`)
-        .setDescription(`Which user's nickname would you like to change? (Admin-Only)`)
-        .setRequired(false)),
+        .addStringOption(option => option.setName(`vr`)
+            .setDescription(`Do you play in vr?`)
+            .setRequired(false)
+            .addChoice("Yes", "yes")
+            .addChoice("No", "no"))
+        .addUserOption(option => option.setName(`user`)
+            .setDescription(`Which user's nickname would you like to change? (Admin-Only)`)
+            .setRequired(false)),
     permissions: 2,
 
     async execute(interaction) {
         let targetUserRoles = interaction.member.roles
 
-        const staffRoles = [  
+        console.log(interaction.member.roles.cache.some(role => role.name === "XB"))
+        
+        const staffRoles = [
             "380249268818018304",
             "380248896385056769",
             "380248192534577152",
@@ -37,12 +39,12 @@ module.exports = {
         for (let role of staffRoles) {
 
             if (targetUserRoles.cache.has(role)) {
-                if (interaction.options.data.find(arg => arg.name ===`user`) !== undefined) {
+                if (interaction.options.data.find(arg => arg.name === `user`) !== undefined) {
                     targetUserRoles = interaction.options.data.find(arg => arg.name === `user`).roles
                     break;
                 }
-            } 
-        } 
+            }
+        }
 
 
         let inGameName = interaction.options.data.find(arg => arg.name === `ign`).value;
@@ -69,27 +71,27 @@ module.exports = {
 
         if (platforms.length === 1) {
             newNickname = `[${platforms[0]}] CMDR ${inGameName}${returnSquadronTag()}`;
-            console.log(`set ${interaction.member.nickname}'s username to ${newNickname}`);
+            console.log(`Set ${interaction.member.nickname}'s username to ${newNickname}`);
 
         } else if (platforms.length === 2) {
             newNickname = `[${platforms[0]}/${platforms[1]}] CMDR ${inGameName}${returnSquadronTag()}`;
-            console.log(`set ${interaction.member.nickname}'s username to ${newNickname}`);
+            console.log(`Set ${interaction.member.nickname}'s username to ${newNickname}`);
 
         } else if (platforms.length === 3) {
 
             newNickname = `[${platforms[0]}/${platforms[1]}/${platforms[2]}] CMDR ${inGameName}${returnSquadronTag()}`;
-            console.log(`set ${interaction.member.nickname}'s username to ${newNickname}`);
+            console.log(`Set ${interaction.member.nickname}'s username to ${newNickname}`);
 
         }
-        
+
         await interaction.member.setNickname(newNickname, "Rule 3")
 
-        interaction.reply({ content: `Your nickname is now ${newNickname}`, ephemeral: true})
+        interaction.reply({ content: `Your nickname is now ${newNickname}`, ephemeral: true })
 
         function returnSquadronTag() {
             if (squadronCode) {
-                return squadronCode
-            } 
+                return `[${squadronCode}]`
+            }
             return ``
         }
     }
