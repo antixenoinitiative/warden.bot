c8c66b8dd0861da0daaad2cac6ec4d0939f3009b
F0rd Pr3f3ct <h00py.fr00d.f0rd.pr3f3ct@gmail.com>
2021-09-21 13:01:02 +0200

    added the whereis command. formatted zen.js

--- commands/fun/zen.js
+++ commands/fun/zen.js
@@ -1,3 +1,9 @@
+/**
+ * A fun command to get a random 'zen' quote from https://zenquotes.io
+ * Note: leads to funny moments as the quote often match the situation :D
+ * 
+ * @author F0rd Pr3f3ct
+ */
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require("node-fetch");
 const { zenURL } = require('../../config.json');
@@ -6,12 +12,11 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName(`zen`)
     .setDescription(`Gets you a random quote from https://zenquotes.io`),
-//    .setDefaultPermission(false),
     permissions: 0,
     execute (interaction) {
         fetch(`${zenURL}`)
-            .then(res => res.json())
-            .then(data => data[0]["q"] + " - *" + data[0]["a"] +"*")
-            .then(quote => interaction.reply({ content: quote }));
+        .then(res => res.json())
+        .then(data => data[0]["q"] + " - *" + data[0]["a"] +"*")
+        .then(quote => interaction.reply({ content: quote }));
 	}
 };
---
+++ commands/sheriff/commons/utils.js
@@ -0,0 +1,46 @@
+/**
+ * @author F0rd Pr3f3ct
+ * A collection of util method uses in several files accross the application.
+ */
+
+exports.dynamicsort = function (property,order) {
+    var sort_order = 1;
+    if(order === "desc"){
+        sort_order = -1;
+    }
+    return function (a, b){
+        // a should come before b in the sorted order
+        if(a[property] < b[property]){
+                return -1 * sort_order;
+        // a should come after b in the sorted order
+        }else if(a[property] > b[property]){
+                return 1 * sort_order;
+        // a and b are the same
+        }else{
+                return 0 * sort_order;
+        }
+    }
+}
+
+exports.isNoneOrEmpty = function (aString){
+    return (!aString || aString.length === 0) ? 'None' : aString;
+}
+
+exports.abbrieviate = function (longName){
+    var tokens = longName.split(' ');
+    return tokens.map((token) => token[0]).join('');
+}
+
+exports.expand = function (abbreviation){
+    switch (abbreviation) {
+        case 'AXI':
+            return "Anti Xeno Initiative";
+            break;
+        case 'XRG':
+            return "Xeno Research Group"
+            break;
+        default:
+            return "Sorry, I do not know the abbreviation '" + abbreviation + "'.";
+            break;
+    }
+}
