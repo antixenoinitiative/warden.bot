55d437422123dbb7af70787ca306e30b56538bc8
Mgram <85346345+CMDRMgram@users.noreply.github.com>
2022-03-06 10:22:15 +1100

    Added async/await

--- commands/fun/malda.js
+++ commands/fun/malda.js
@@ -6,12 +6,14 @@ module.exports = {
     .setDescription(`Annoy MalzaCAr with pings`),
     permissions: 0,
 
-    execute(interaction) {
-        if (interaction.member.id == "585889289724755989") interaction.reply({content: "<@585889289724755989>"}); //585889289724755989 doge's id
+    async execute(interaction) {
+        if (interaction.member.id == "585889289724755989") {
+            await interaction.reply({content: "<@585889289724755989>"}); //585889289724755989 doge's id
+        }
 
         else {
-            interaction.reply({content: "<@274853598280810496> AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"});
-            interaction.channel.send({content: 'https://cdn.discordapp.com/attachments/763535317360705606/919244730691358770/200w.gif'});
+            await interaction.reply({content: "<@274853598280810496> AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"});
+            await interaction.channel.send({content: 'https://cdn.discordapp.com/attachments/763535317360705606/919244730691358770/200w.gif'});
         }
     }
 }
