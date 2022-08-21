b0e81ce0e92704f710455e0a57cf777e64d8de2f
Mgram <85346345+CMDRMgram@users.noreply.github.com>
2022-08-21 11:16:48 +1000

    Update ranks.js

--- commands/admin/ranks.js
+++ commands/admin/ranks.js
@@ -11,7 +11,7 @@ module.exports = {
 		.setColor('#FF7100')
 		.setTitle("**Rank Requirements**")
 		.setAuthor({ name: 'Anti-Xeno Initiative', iconURL: 'https://axicdn.s3.us-east-1.amazonaws.com/images/AXI_Insignia_Hypen_128.png', url: 'https://antixenoinitiative.com' })
-		.setThumbnail('https://axicdn.s3.us-east-1.amazonaws.com/images/AXI_Insignia_Hypen_128.png')
+		.setThumbnail('https://axicdn.s3.us-east-1.amazonaws.com/images/club10.png')
 		.setDescription(`The Anti-Xeno Initiative uses ranks to encourage CMDRs to develop and prove their skills as an AX Pilot. Learn all about the various challenges and competitive ranks you can earn in the AXI. 
 
 Please click the link below to view our Ranks website to find all the up-to-date information on how to earn your first AX combat rank. This can also be found by visiting our website at www.antixenoinitiative.com.`)
