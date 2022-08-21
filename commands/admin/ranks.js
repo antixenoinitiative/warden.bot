780a1b4f55e0d197a3930fc413c0ff8d926cd585
Mgram <85346345+CMDRMgram@users.noreply.github.com>
2022-08-21 11:09:37 +1000

    Create ranks.js

---
+++ commands/admin/ranks.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName(`rankrequirements`)
+    .setDescription(`Create the Rank Requirement buttons`)
+    .setDefaultPermission(false),
+    async execute (interaction) {
+        const returnEmbed = new Discord.MessageEmbed()
+		.setColor('#FF7100')
+		.setTitle("**Rank Requirements**")
+		.setDescription(`The Anti-Xeno Initiative uses ranks to encourage CMDRs to develop and prove their skills as an AX Pilot. Learn all about the various challenges and competitive ranks you can earn in the AXI. 
+
+Please click the link below to view our Ranks website to find all the up-to-date information on how to earn your first AX combat rank. This can also be found by visiting our website at www.antixenoinitiative.com.`)
+
+        const row = new Discord.MessageActionRow()
+        .addComponents(new Discord.MessageButton().setLabel('View Rank Requirements').setStyle('LINK').setURL('https://antixenoinitiative.com/ranks'),)
+
+        interaction.channel.send({ embeds: [returnEmbed], components: [row] });
+    }
+}
