aa80aae0f977a10ec913ae598606122698023b72
MalzaCAr <88378003+MalzaCAr@users.noreply.github.com>
2022-10-15 12:04:07 +0200

    Quick /malda update
    Sardine is no more

--- commands/fun/malda.js
+++ commands/fun/malda.js
@@ -7,14 +7,16 @@ module.exports = {
     permissions: 0,
 
     async execute(interaction) {
-        if (interaction.member.id == "585889289724755989") {
-            await interaction.reply({content: "<@585889289724755989>"}); //585889289724755989 doge's id
-        }
-
-        else {
-            await interaction.reply( {content: `<@274853598280810496> AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 
+        switch (interaction.member.id) {
+            case 585889289724755989:
+                interaction.reply({content: "<@585889289724755989>"}); //585889289724755989 doge's id
+                break;
+            case 231298589148577792:
+                interaction.reply({content: "No more, <@231298589148577792>"});
+                break;
+            default:
+                await interaction.reply( {content: `<@274853598280810496> AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 
 https://cdn.discordapp.com/attachments/763535317360705606/919244730691358770/200w.gif`} );
-
         }
     }
 }
