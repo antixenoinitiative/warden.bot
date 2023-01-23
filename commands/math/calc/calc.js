417c2d0f3a6eaf375f236d9763c403bb1fbf72d1
airom <46094325+Airom42@users.noreply.github.com>
2023-01-23 03:09:43 -0500

    optimize mttot

--- commands/math/calc/calc.js
+++ commands/math/calc/calc.js
@@ -1,50 +1,17 @@
 const thargoids = require("./thargoiddata.json")
 const weaponData = require("./weapondata.json")
 
-function arrayTotal(arr) {
-    var total = 0;
-    for(var i in arr) { total += arr[i]; }
-    return total
-}
-
-function calcDPS(target, inputcode, range) {
-    let multi;
-    //if (range = undefined) { range = 1500 }
+function calcDPS(target, multi, inputcode, range) {
     try {
-        if (inputcode.match(/^\d/)) {
-            multi = inputcode.charAt(0); // Get Multiplier
-            inputcode = inputcode.substring(1); // Remove Multiplier from code
-        }
+        const data = weaponData[inputcode];
+        let nDPS = (data.axsdps + data.humansdps * 0.01) * multi;
+        let DPS = nDPS * data.ap / thargoids[target].ar;
+        let finaldamage = DPS * Math.min((1 - ((range - data.falloff) / (data.maxrange - data.falloff))), 1)
 
-        switch (inputcode) {
-            case "mgauss":
-                inputcode = "mfgc";
-                break;
-            case "sgauss":
-                inputcode = "sfgc";
-                break;
-            case "mfgauss":
-                inputcode = "mfgc";
-                break;
-            case "sfgauss":
-                inputcode = "sfgc";
-                break;
-            case "m":
-                inputcode = "mfgc";
-                break;
-            case "s":
-                inputcode = "sfgc";
-                break;
-        }
+        let finaldamagestd = finaldamage * data.stdammopercent
+        let finaldamageprem = finaldamage * data.premammopercent
 
-        let nDPS = (weaponData[inputcode].axsdps + weaponData[inputcode].humansdps * 0.01) * multi;
-        let DPS = nDPS * weaponData[inputcode].ap / thargoids[target].ar;
-        let finaldamage = DPS * Math.min((1 - ((range - weaponData[inputcode].falloff) / (weaponData[inputcode].maxrange - weaponData[inputcode].falloff))), 1)
-
-        let finaldamagestd = finaldamage * weaponData[inputcode].stdammopercent
-        let finaldamageprem = finaldamage * weaponData[inputcode].premammopercent
-
-        let result = { "basic": finaldamage, "standard": finaldamagestd, "premium": finaldamageprem }
+        let result = { "basic": finaldamage, "standard": finaldamagestd, "premium": finaldamageprem, "cycleTime":data.cycletime }
         return result;
 
     } catch (err) {
@@ -55,59 +22,49 @@ function calcDPS(target, inputcode, range) {
 }
 
 module.exports = {
-    calcMTTOT: (target, weapons, range) => {
+    calcMTTOT: (target, weapons, range, accuracy) => {
         try {
-            if (range == undefined) { range = 1500 }
-
-            let basic = [];
-            let standard = [];
-            let premium = [];
-
-
+            let basicTotal = 0;
+            let standardTotal = 0;
+            let premiumTotal = 0;
+            let minCycle = 10;
 
             // Calculate each weapon adjDPS
             for (let i = 0; i < weapons.length; i++) {
-                let ans = calcDPS(target, weapons[i], range)
-                basic.push(ans.basic)
-                standard.push(ans.standard)
-                premium.push(ans.premium)
+                let ans = calcDPS(target, weapons[i].number, weapons[i].code, range)
+                basicTotal += ans.basic;
+                standardTotal += ans.standard;
+                premiumTotal += ans.premium;
+                if(ans.cycleTime < minCycle) minCycle = ans.cycleTime;
             }
             
-            let basicTotal = arrayTotal(basic);
-            let standardTotal = arrayTotal(standard);
-            let premiumTotal = arrayTotal(premium);
-            
-            let mttotArray = [] // [ basic100, std100, prem100, basic75, std75, prem75, basic50, std50, prem50 ]
+            let mttotArray = [] // [ basic, standard, premium ]
 
-            mttotArray.push(thargoids[target].hp / (basicTotal - thargoids[target].regen))              // basic100
-            mttotArray.push(thargoids[target].hp / (standardTotal - thargoids[target].regen))           // std100 
-            mttotArray.push(thargoids[target].hp / (premiumTotal - thargoids[target].regen))            // prem100
-            mttotArray.push(thargoids[target].hp / (basicTotal / 4 * 3 - thargoids[target].regen))      // basic75
-            mttotArray.push(thargoids[target].hp / (standardTotal / 4 * 3 - thargoids[target].regen))   // std75
-            mttotArray.push(thargoids[target].hp / (premiumTotal / 4 * 3 - thargoids[target].regen))    // prem75
-            mttotArray.push(thargoids[target].hp / (basicTotal / 2 - thargoids[target].regen))          // basic50
-            mttotArray.push(thargoids[target].hp / (standardTotal / 2 - thargoids[target].regen))       // std50
-            mttotArray.push(thargoids[target].hp / (premiumTotal / 2 - thargoids[target].regen))        // prem50
+            // t = [ HP_to_exert - ( weapon_cycle_time * goid_regen ) ] / (weapons_sdps - goid_regen)
+            const goidRegen = thargoids[target].regen;
+
+            mttotArray.push((thargoids[target].hp - (minCycle * goidRegen)) / (basicTotal * accuracy - goidRegen));
+            mttotArray.push((thargoids[target].hp - (minCycle * goidRegen)) / (standardTotal * accuracy - goidRegen));
+            mttotArray.push((thargoids[target].hp - (minCycle * goidRegen)) / (premiumTotal * accuracy - goidRegen));
 
             let result = []
             for (let i=0; i < mttotArray.length; i++) {
                 if (mttotArray[i] >= 120) {
                     result[i] = `☠️ ${mttotArray[i].toFixed(2)}sec`
-                } else if (mttotArray[i] >= 50) {
+                } else if (mttotArray[i] >= 60) {
                     result[i] = `🟥 ${mttotArray[i].toFixed(2)}sec`
+                } else if (mttotArray[i] >= 45) {
+                    result[i] = `🟧 ${mttotArray[i].toFixed(2)}sec`
                 } else if (mttotArray[i] >= 30) {
                     result[i] = `🟨 ${mttotArray[i].toFixed(2)}sec`
                 } else {
                     result[i] = `🟩 ${mttotArray[i].toFixed(2)}sec`
                 }
-
                 if (result[i].includes("-")) {
                     result[i] = `Insufficient DPS`
                 }
             }
-            
             return result
-
         } catch (err) {
             console.log(err);
         }
