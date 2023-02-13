5c605425244110b3f3f7fe5e520aa9ff08406dbf
Airom <46094325+Airom42@users.noreply.github.com>
2023-02-13 13:47:53 -0500

    change order of engineering.js

--- commands/info/engineering.js
+++ commands/info/engineering.js
@@ -11,11 +11,11 @@ module.exports = {
         .setAuthor({name: 'Anti-Xeno Initiative',iconURL: config.icon})
         .setThumbnail("https://edassets.org/static/img/engineers/Engineer_icon.png")
         .addFields(
-            {name: "ED Materials", value: "https://www.edmaterials.app\nComprehensive guide on where and how to farm engineering materials. This is the single best resource for engineering in the game." },
-            {name: "AXI Horizons Engineering", value: "https://wiki.antixenoinitiative.com/en/engineering\nAXI's engineering material guide."},
-            {name: "AX Engineer Unlock Guide", value: "https://wiki.antixenoinitiative.com/en/Unlocking-Engineers\nAXI's guide for unlocking engineers."},
-            {name: "AXI Odyssey Engineering", value: "https://wiki.antixenoinitiative.com/en/engineering-odyssey\nAXI's guide to odyssey engineering materials."},
+            {name: "ED Materials", value: "https://www.edmaterials.app\nComprehensive guide on where and how to farm engineering materials. This is the single best resource for engineering." },
+            {name: "AX Engineer Unlock Guide", value: "https://wiki.antixenoinitiative.com/en/Unlocking-Engineers\nAXI's guide for unlocking engineers and how to engineer."},
             {name: "Engineering and Module Unlock Costs", value: "https://inara.cz/elite/techbroker/\nList of module unlock costs and engineering roll costs."},
+            {name: "AXI Odyssey Engineering", value: "https://wiki.antixenoinitiative.com/en/engineering-odyssey\nAXI's guide to odyssey engineering materials."},
+            {name: "AXI Horizons Engineering", value: "https://wiki.antixenoinitiative.com/en/engineering\nAXI's engineering material guide."},
         )
         interaction.reply({embeds: [returnEmbed.setTimestamp()]})
     }
