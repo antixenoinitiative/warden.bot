b577e7b243a03b2e45bc88600e783c91500e46dc
Airom <46094325+Airom42@users.noreply.github.com>
2023-02-15 12:13:09 -0500

    update interceptor cards

--- commands/interceptorcards/hydra.js
+++ commands/interceptorcards/hydra.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription(`Hydra info card`),
     permissions: 0,
     execute (interaction) {
-        interaction.reply({ content: "https://cdn.discordapp.com/attachments/763535317360705606/808376895925649428/Hydra.png" });
+        interaction.reply({ content: "https://cdn.discordapp.com/attachments/955583721883467787/1075228406528090123/Hydra.png" });
     }
 }
