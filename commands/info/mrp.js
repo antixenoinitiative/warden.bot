d912156ce7fc3111c204348614ffd5da624fff4e
LaCrak 27 <ivancrak123@gmail.com>
2023-03-30 15:33:20 +0200

    Updated /mrp command to show better info

--- commands/info/mrp.js
+++ commands/info/mrp.js
@@ -13,8 +13,10 @@ module.exports = {
         .setThumbnail('https://static.wikia.nocookie.net/elite-dangerous/images/9/96/MRP.png/revision/latest?cb=20170114223512')
         .setDescription(`Multiple MRPs will combine their module protection %, but always take damage in a set order:
 
-        - Optional Slots before Military Slots
-        - Largest to smallest
+        - MRPs are critically important, you will want at least 3 on shieldless ships and 2 on shielded ships
+        - (The stuff around largest taking damage first, which is one "one large, two small" is the advice for MRPs)
+        - The caution about milslots - use milslots for HRPs, not MRPs
+        - The fact the guardian version is a bit higher integrity, but needs power, so net-net the regular ones are fine
         
         Always ensure your largest MRP is in an Optional slot or your module protection will be compromised much sooner.`)
         .setFooter({ text: 'Module Reinforcement Package', iconURL: config.icon });
