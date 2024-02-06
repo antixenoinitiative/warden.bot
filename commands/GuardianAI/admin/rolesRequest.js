142eb21a5e1f8ce77c2ba282d90cb3dda8891ce2
testfax <testfax.medi0cr3@gmail.com>
2024-02-05 21:20:25 -0700

    roles req comment out

--- commands/GuardianAI/admin/rolesRequest.js
+++ commands/GuardianAI/admin/rolesRequest.js
@@ -1,42 +1,42 @@
-const Discord = require("discord.js");
-const {requestInfo} = require('../../../socket/taskManager')
+// const Discord = require("discord.js");
+// const {requestInfo} = require('../../../socket/taskManager')
 
-module.exports = {
-    data: new Discord.SlashCommandBuilder()
-        .setName('roles_req')
-        .setDescription('Select player')
-        .addUserOption(option =>
-            option
-                .setName('user')
-                .setDescription('The user you want to check roles on from multiple configured servers.')
-                .setRequired(true)
-        ),
+// module.exports = {
+//     data: new Discord.SlashCommandBuilder()
+//         .setName('roles_req')
+//         .setDescription('Select player')
+//         .addUserOption(option =>
+//             option
+//                 .setName('user')
+//                 .setDescription('The user you want to check roles on from multiple configured servers.')
+//                 .setRequired(true)
+//         ),
     
-    permissions: 0,
-    async execute(interaction) {
-        if (process.env.SOCKET_TOKEN) { 
-            try {
-                const { options } = interaction
-                let person_asking = interaction.user.id
-                const subject = options.getUser('user')
-                const member = guild.members.cache.get(subject.id)
-                let roles = member.roles.cache.map(role=>role.name)
-                roles = roles.filter(x=>x != '@everyone')
-                let rolePackage = { 
-                    type: "roles_request",
-                    user: subject, 
-                    roles: roles,
-                    person_asking: person_asking 
-                }
-                requestInfo(rolePackage)
-                return interaction.reply({ content:`Checking roles of ${rolePackage.user} on other configured servers.`, ephemeral: true })
-            }
-            catch(e) {
-                console.log(e)
-            }
-        }
-        else {
-            return interaction.reply({ content:`Socket Token not configured. Contact administrator.`, ephemeral: true })
-        }
-    } 
-};
+//     permissions: 0,
+//     async execute(interaction) {
+//         if (process.env.SOCKET_TOKEN) { 
+//             try {
+//                 const { options } = interaction
+//                 let person_asking = interaction.user.id
+//                 const subject = options.getUser('user')
+//                 const member = guild.members.cache.get(subject.id)
+//                 let roles = member.roles.cache.map(role=>role.name)
+//                 roles = roles.filter(x=>x != '@everyone')
+//                 let rolePackage = { 
+//                     type: "roles_request",
+//                     user: subject, 
+//                     roles: roles,
+//                     person_asking: person_asking 
+//                 }
+//                 requestInfo(rolePackage)
+//                 return interaction.reply({ content:`Checking roles of ${rolePackage.user} on other configured servers.`, ephemeral: true })
+//             }
+//             catch(e) {
+//                 console.log(e)
+//             }
+//         }
+//         else {
+//             return interaction.reply({ content:`Socket Token not configured. Contact administrator.`, ephemeral: true })
+//         }
+//     } 
+// };
