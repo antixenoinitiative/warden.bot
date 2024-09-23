9cc5cb9d1afddadc41a2cbadaa1cef9c8df0b878
HansLuft778 <hendrik.lante@gmail.com>
2024-09-23 12:16:04 +0200

    Changes `pg` command to use config for rules channel

--- commands/common/pg.js
+++ commands/common/pg.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { botIdent } = require('../../functions');
+const config = require('../../config.json');
 module.exports = {
     data: new Discord.SlashCommandBuilder()
     .setName(`pg`)
@@ -7,6 +8,9 @@ module.exports = {
     // .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     permissions:0,
     execute (interaction) {
+        const channels = interaction.guild.channels.cache;
+        const rulesChannelId = channels.find(channel => channel.name === config.Warden.channels.privateGroupRules).id;
+
         const returnEmbed = new Discord.EmbedBuilder()
         .setTitle(`${botIdent().activeBot.communityName} Private Group`)
         .setColor('#FF7100')
@@ -19,7 +23,7 @@ module.exports = {
             `3. Select the "${botIdent().activeBot.communityName}" and click "Request to join private group"\n` +
             `4. The Request will be automatically approved\n` +
             `5. Return to the menu, select Start > Private Group > ${botIdent().activeBot.communityName} > Join Group\n` +
-            `\nPlease read the Private Group Rules before joining: <#1054193210324439082>`
+            `\nPlease read the Private Group Rules before joining: <#${rulesChannelId}>`
         )
         .setFooter({ text: `Joining ${botIdent().activeBot.communityName} Private Group`, iconURL: botIdent().activeBot.icon });
         interaction.reply({embeds: [returnEmbed]})
