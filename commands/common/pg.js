550d4b62ec1403d9f9f62b09d87341d2bce32f8a
HansLuft778 <hendrik.lante@gmail.com>
2024-09-23 12:48:23 +0200

    Refactor `pg` command to use botIdent

--- commands/common/pg.js
+++ commands/common/pg.js
@@ -3,29 +3,31 @@ const { botIdent } = require('../../functions');
 const config = require('../../config.json');
 module.exports = {
     data: new Discord.SlashCommandBuilder()
-    .setName(`pg`)
-    .setDescription(`Posts info on how to join the ${botIdent().activeBot.communityName} Private Group`),
+        .setName(`pg`)
+        .setDescription(`Posts info on how to join the ${botIdent().activeBot.communityName} Private Group`),
     // .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-    permissions:0,
-    execute (interaction) {
+    permissions: 0,
+    execute(interaction) {
         const channels = interaction.guild.channels.cache;
-        const rulesChannelId = channels.find(channel => channel.name === config.Warden.channels.privateGroupRules).id;
+        const rulesChannelId = channels.find(
+            channel => channel.name === config[botIdent().activeBot.botName].channels.privateGroupRules
+        ).id;
 
         const returnEmbed = new Discord.EmbedBuilder()
-        .setTitle(`${botIdent().activeBot.communityName} Private Group`)
-        .setColor('#FF7100')
-        .setAuthor({name: botIdent().activeBot.botName,iconURL: botIdent().activeBot.icon})
-        .setThumbnail(botIdent().activeBot.icon)
-        .setDescription(
-            `**How to join the Private Group**\n` +
-            `1. Open the Social Menu (Menu > Social)\n` +
-            `2. On the Friends tab, use the search box to find "${botIdent().activeBot.communityName}".\n` +
-            `3. Select the "${botIdent().activeBot.communityName}" and click "Request to join private group"\n` +
-            `4. The Request will be automatically approved\n` +
-            `5. Return to the menu, select Start > Private Group > ${botIdent().activeBot.communityName} > Join Group\n` +
-            `\nPlease read the Private Group Rules before joining: <#${rulesChannelId}>`
-        )
-        .setFooter({ text: `Joining ${botIdent().activeBot.communityName} Private Group`, iconURL: botIdent().activeBot.icon });
-        interaction.reply({embeds: [returnEmbed]})
+            .setTitle(`${botIdent().activeBot.communityName} Private Group`)
+            .setColor('#FF7100')
+            .setAuthor({ name: botIdent().activeBot.botName, iconURL: botIdent().activeBot.icon })
+            .setThumbnail(botIdent().activeBot.icon)
+            .setDescription(
+                `**How to join the Private Group**\n` +
+                `1. Open the Social Menu (Menu > Social)\n` +
+                `2. On the Friends tab, use the search box to find "${botIdent().activeBot.communityName}".\n` +
+                `3. Select the "${botIdent().activeBot.communityName}" and click "Request to join private group"\n` +
+                `4. The Request will be automatically approved\n` +
+                `5. Return to the menu, select Start > Private Group > ${botIdent().activeBot.communityName} > Join Group\n` +
+                `\nPlease read the Private Group Rules before joining: <#${rulesChannelId}>`
+            )
+            .setFooter({ text: `Joining ${botIdent().activeBot.communityName} Private Group`, iconURL: botIdent().activeBot.icon });
+        interaction.reply({ embeds: [returnEmbed] })
     }
 }
