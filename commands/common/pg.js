3259891fcb3cb106ac9d0e499844ca452eaafd0c
HansLuft778 <hendrik.lante@gmail.com>
2024-09-23 11:34:17 +0200

    pg: removed indentation levels and added link to pg rules

--- commands/common/pg.js
+++ commands/common/pg.js
@@ -12,12 +12,15 @@ module.exports = {
         .setColor('#FF7100')
         .setAuthor({name: botIdent().activeBot.botName,iconURL: botIdent().activeBot.icon})
         .setThumbnail(botIdent().activeBot.icon)
-        .setDescription(`**How to join the Private Group**
-                            1. Open the Social Menu (Menu > Social)
-                            2. On the Friends tab, use the search box to find "${botIdent().activeBot.communityName}".
-                            3. Select the "${botIdent().activeBot.communityName}" and click "Request to join private group"
-                            4. The Request will be automatically approved
-                            5. Return to the menu, select Start > Private Group > ${botIdent().activeBot.communityName} > Join Group`)
+        .setDescription(
+            `**How to join the Private Group**\n` +
+            `1. Open the Social Menu (Menu > Social)\n` +
+            `2. On the Friends tab, use the search box to find "${botIdent().activeBot.communityName}".\n` +
+            `3. Select the "${botIdent().activeBot.communityName}" and click "Request to join private group"\n` +
+            `4. The Request will be automatically approved\n` +
+            `5. Return to the menu, select Start > Private Group > ${botIdent().activeBot.communityName} > Join Group\n` +
+            `\nPlease read the Private Group Rules before joining: <#1054193210324439082>`
+        )
         .setFooter({ text: `Joining ${botIdent().activeBot.communityName} Private Group`, iconURL: botIdent().activeBot.icon });
         interaction.reply({embeds: [returnEmbed]})
     }
