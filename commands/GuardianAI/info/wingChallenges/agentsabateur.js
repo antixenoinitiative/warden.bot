8d86b149072aa66065a98fbdb31f49218752542d
Medi0cr3 <testfax.medi0cr3@gmail.com>
2024-10-10 22:01:47 -0700

    update

--- commands/GuardianAI/info/wingChallenges/agentsabateur.js
+++ commands/GuardianAI/info/wingChallenges/agentsabateur.js
@@ -2,25 +2,22 @@ const Discord = require("discord.js");
 const { botIdent } = require('../../../../functions');
 module.exports = {
     data: new Discord.SlashCommandBuilder()
-    .setName(`agentsabateur`)
+    .setName(`agentsaboteur`)
     .setDescription(`Display Information`),
     execute (interaction) {
         const returnEmbed = new Discord.EmbedBuilder()
-        .setTitle(`Agent Sabatuer`)
+        .setTitle(`Agent Sabotuer`)
         .setColor('#FF7100')
         .setAuthor({name: botIdent().activeBot.botName,iconURL: botIdent().activeBot.icon})
         .setThumbnail(botIdent().activeBot.icon)
-        .setDescription(`**How to join the Private Group**
-                            1. Open the Social Menu (Menu > Social)
-                            2. On the Friends tab, use the search box to find "XSF Official".
-                            3. Select the "XSF Official" and click "Request to join private group"
-                            4. The Request will be automatically approved
-                            5. Return to the menu, select Start > Private Group > XSF Official > Join Group
-                            Its also recommended to send a friend request to "XSF Official" incase the PG gets locked down.
-                            `
-                            
-                        )
-        .setFooter({ text: `Joining XSF Official private group`, iconURL: botIdent().activeBot.icon });
-        interaction.reply({embeds: [returnEmbed]})
+        .setDescription(`Agent Saboteur Information`)
+        .addFields(
+            { name: "Title:", value: "Fully sabotage a Thargoid Spire Site in a wing of 2 to 4 CMDRs.", inline: false },
+            { name: "Special:", value: "N/A", inline: false },
+        )
+        .setFooter({ text: `Agent Saboteur`, iconURL: botIdent().activeBot.icon })
+        const buttonRow = new Discord.ActionRowBuilder()
+                .addComponents(new Discord.ButtonBuilder().setLabel('Visit XSF website for Agent Saboteur Requirements').setStyle(Discord.ButtonStyle.Link).setURL('https://xenostrikeforce.com/?page_id=1207#wing-combat-ranks'),)
+        interaction.reply({ components: [buttonRow], embeds: [returnEmbed] })
     }
 }
